0b4cc60e05e512cc57e4c396266ede0f4acd68c0
thatsfov1 <zheniaukr@icloud.com>
2023-01-05 22:28:53 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from "./components/Header/Header.jsx";
+
+function App() {
+
+  return (
+      <div>
+          <Header/>
+          <div className="app">
+
+          </div>
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './Header.module.css'
+
+const Header = (props) => {
+    return <div className={s.header}>
+        <h1>
+            Quiz App
+        </h1>
+    </div>;
+}
+
+export default Header;
