6cb76fa2589567d5cad1f494c0f4e81cc4b5bda5
thatsfov1 <zheniaukr@icloud.com>
2023-01-07 13:11:29 +0200

    fixed browser router v2

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,38 @@ import Quiz from "./components/Pages/Quiz/Quiz.jsx";
 import Results from "./components/Pages/Results/Results.jsx";
 import {useState} from "react";
 import axios from "axios";
+import {Container} from "@mui/material";
 
 function App() {
 
 
-    const [name,setName] = useState('')
-    const [questions,setQuestions] = useState('')
-    const [score,setScore] = useState(0)
+    const [name, setName] = useState('')
+    const [questions, setQuestions] = useState('')
+    const [score, setScore] = useState(0)
 
-    const fetchQuestions = async(category = '',difficulty = '') =>{
+    const fetchQuestions = async (category = '', difficulty = '') => {
         const {data} = await axios.get(`https://opentdb.com/api.php?amount=20&category=${category}&difficulty=${difficulty}&type=multiple`)
         setQuestions(data.results)
     }
 
-  return (
-          <BrowserRouter>
-              <Header/>
-              <div className="app">
-                  <Routes>
-                      <Route path={'/'} exact element={<Home fetchQuestions={fetchQuestions} name={name} setName={setName}/>}/>
-                      <Route path={'/quiz'} element={<Quiz score={score} setScore={setScore}
-                                                           name={name} questions={questions} setQuestions={setQuestions}/>}/>
-                      <Route path={'/result'} element={<Results name={name} score={score}/>}/>
-                  </Routes>
-              </div>
-          </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Header/>
+            <div className="app">
+
+                <Container>
+                    <Routes>
+                        <Route path={'/quiz-app/'} exact
+                               element={<Home fetchQuestions={fetchQuestions} name={name} setName={setName}/>}/>
+                        <Route path={'/quiz-app/quiz'} element={<Quiz score={score} setScore={setScore}
+                                                             name={name} questions={questions}
+                                                             setQuestions={setQuestions}/>}/>
+                        <Route path={'/quiz-app/result'} element={<Results name={name} score={score}/>}/>
+                    </Routes>
+                </Container>
+            </div>
+        </BrowserRouter>
+    )
 }
 
 export default App
